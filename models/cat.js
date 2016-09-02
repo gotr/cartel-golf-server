6849861a6f64f199169dcc83dcfe217897998d7d
jim-clark <email@jim-clark.com>
2016-09-02 12:25:46 -0700

    Testing mongodb connection on localhost

---
+++ models/cat.js
@@ -0,0 +1,5 @@
+var mongoose = require('mongoose');
+
+var Cat = mongoose.model('Cat', {name: String});
+
+module.exports = Cat;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var Cat = require('../models/cat');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var kitty = new Cat({name: 'cat ' + Date.now().toString()});
+  kitty.save(function() {
+    res.render('index', { title: 'Express', cat: kitty });
+  });
 });
 
 module.exports = router;
