7803501f2a9d7abf83ea0a88137d171b80060211
jim-clark <email@jim-clark.com>
2016-09-03 10:38:48 -0700

    Improve mongodb connection message to the console

--- database/database.js
+++ database/database.js
@@ -3,5 +3,5 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
 
 mongoose.connection.once('open', function(){
-  console.log('connected to mongodb:', mongoose.connection);
+  console.log(`Connected to mongodb\nserver: ${mongoose.connection.host}:${mongoose.connection.port}\ndatabse: ${mongoose.connection.name}`);
 });
