ee46f0d928422830eb57085cf59585e93e42ce09
jim-clark <email@jim-clark.com>
2016-09-06 16:36:22 -0700

    Change database from 'test' to 'cartel'

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://localhost/cartel');
 
 mongoose.connection.once('open', function(){
   console.log(`Connected to mongodb\nserver: ${mongoose.connection.host}:${mongoose.connection.port}\ndatabse: ${mongoose.connection.name}`);
