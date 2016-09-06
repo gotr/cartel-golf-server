9274f77baabd8911a3b3b061cb149729c522fb17
jim-clark <email@jim-clark.com>
2016-09-06 16:45:13 -0700

    Remove cat model and related testing stuff

--- models/cat.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose');
-
-var Cat = mongoose.model('Cat', {name: String});
-
-module.exports = Cat;
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-var Cat = require('../models/cat');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var kitty = new Cat({name: 'cat ' + Date.now().toString()});
-  kitty.save(function() {
-    res.render('index', { title: 'Express', cat: kitty });
-  });
+  res.render('index', { title: 'Cartel Golf' });
 });
 
 module.exports = router;
