2ae8fbc0896a24e68569de2bfe1035a0fb772b92
jim-clark <email@jim-clark.com>
2016-09-07 17:19:18 -0700

    Update seed.js

--- database/seed.js
+++ database/seed.js
@@ -14,8 +14,33 @@ Promise.all([
 ])
 .then(() => {
   console.log('Deleted all Courses, Users & Cartels removed');
+  return Course.create({
+    name: 'South Hills Country Club',
+    abbreviation: 'SHCC'
+  }, (err, doc) => {
+    course = doc;
+  });
 })
 .then(() => {
+  console.log('Created Course: SHCC');
+  return Cartel.create({
+    name: 'The Cartel',
+    defaultCourse: course
+  }, (err, doc) => {
+    cartel = doc;
+  });
+})
+.then(() => {
+  console.log('Created Cartel: The Cartel');
+  return User.create({
+    nickname: 'Clarkie',
+    cartel: cartel
+  }, (err, doc) => {
+    user = doc;
+  });
+})
+.then(() => {
+  console.log('Created User: Clarkie');
   mongoose.disconnect(() => {
     console.log('Disconnected from db');
   });
