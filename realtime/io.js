c254d372c0697a6b2637ee3b3757cab052749e6a
jim-clark <email@jim-clark.com>
2016-09-09 12:58:21 -0700

    Progress on sign-on message in io.js

--- realtime/io.js
+++ realtime/io.js
@@ -1,11 +1,32 @@
 var io = require('socket.io')();
+var messageTypes = require('./message-types');
+
+var User = require('../models/user');
+
+var connectedUsers = {};
 
 // Listen for new connections from clients (socket)
 io.on('connection', function (socket) {
   
-  // all-data: return all data for the cartel the userToken
-  socket.on('all-data', token => {
-    socket.emit('all-data', {data: 'you sent: ' + token });
+  socket.on(messageTypes.signIn, token => {
+    User.findById(token, (err, user) => {
+      if (!err) {
+        if (user.active) {
+          socket.user = user;
+          connectedUsers[user.id] = Date.now();
+          // return object with headers for past rounds for the user's cartel
+          // and a complete pendingRound if there is one
+          socket.emit(messageTypes.setRound, {
+            rounds: [],
+            pendingRound: null
+          })
+        } else {
+          // user has been deactivated
+          // send message that will remove the token from the device
+          socket.emit(messageTypes.deactivate);
+        }
+      }
+    });
   });
 
 });
